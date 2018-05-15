31137834a0a16a0dfb55fbe5215ef5878da48b9d
nszy007 <nszy007@gmail.com>
2018-05-15 09:28:58 +0800

    优化代码

--- water.py
+++ water.py
@@ -13,8 +13,7 @@ money = float(input("Please input the money(0-255):"));
 if money > 255.0 or money < 0.0:       #限定输入范围，超出读卡器不识别
     print("Out of range!");
     exit();
-money = money * 100;
-money = money + 100000;
+money = money * 100 + 10000;
 str_money = struct.pack("i", int(money)).encode("hex");
 str_verif = struct.pack("i", ~int(money)).encode("hex");
 str_out = str_money + str_verif + str_money + '00FF00FF';
