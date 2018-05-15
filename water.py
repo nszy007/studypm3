2d1ae6c2da4da8810fa91cce79d1626a2e4e2624
nszy007 <nszy007@gmail.com>
2018-05-15 09:20:11 +0800

    优化代码

--- water.py
+++ water.py
@@ -2,42 +2,22 @@
 # -*- coding:utf8 -*-
 # 家里水卡数据算法
 # 数据保存在第8块,数据算法如下:
-# 1.a=dword长度取字节集(金额×100+10000)
-# 2.b=a逐byte取反
-# 3.结果等于a+b+a+'00FF00FF'
+# 1.a=金额×100+10000 类型为dword
+# 2.b=a取反 类型为dword
+# 3.结果等于a.取字节集+b.取字节集+a.取字节集+'00FF00FF'
 
+import struct
 
-def conver(numStr):     #十六进制byte长度取反运算
-    strVerif = '';
-    tmpnum = int(numStr, 16);
-    tmpnum = ~tmpnum;
-    tmpnum = tmpnum & 0xff;
-    strVerif = hex(tmpnum);
-    strVerif = strVerif.replace('0x','');
-    return strVerif;
-
-
-money = 0.0;
 money = float(input("Please input the money(0-255):"));
 if money > 255.0:
-    print("Out of range");
+    print("Out of range!");
     exit();
 money = money * 100;
 money = money + 100000;
-str = hex(int(money));
-i = len(str);
-meneyOut = '';
-verif = '';
-while i//2 > 0:
-    tmp1 = str[i-2:i];
-    if tmp1[0:1] == 'x':
-        tmp1 = '0' + tmp1[1:2];
-    tmp2 = conver(tmp1);
-    meneyOut = meneyOut + tmp1;
-    verif = verif + tmp2;
-    i = i - 2;
+str_money = struct.pack("i", int(money)).encode("hex");
+str_verif = struct.pack("i", ~int(money)).encode("hex");
+str_out = str_money + str_verif + str_money + '00FF00FF';
 
-strOut = meneyOut + '00' + verif + 'FF' + meneyOut + '0000FF00FF'
 
 print("Please modiy the 8th block as follows:");
-print(strOut.upper());
+print(str_out.upper());
